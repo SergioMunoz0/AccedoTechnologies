a2806d046f4b687e5862d205952ba6f6bfe2a279
Sergio Carrillo <checho223076@hotmail.com>
2024-08-28 09:36:04 -0500

    layout del componente createPost terminado y ajustes menores a los demas

--- klipclub/src/components/CreatePost.js
+++ klipclub/src/components/CreatePost.js
@@ -1,9 +1,15 @@
 import {useState} from 'react'
 import "../styles/css/utils.css"
 import "../styles/css/createPost.css"
+
+//images and icons
 import profilePicture from '../assets/images/profilePhoto.jpg'
 import arrowDown from '../assets/icons/arrowDown.png'
 import arrowUp from '../assets/icons/arrowUp.png'
+import gallery from '../assets/icons/gallery.png'
+import play from '../assets/icons/play.png'
+import microphone from '../assets/icons/microphone.png'
+import label from '../assets/icons/label.png'
 
 export default function CreatePost(props){
 
@@ -70,6 +76,13 @@ export default function CreatePost(props){
     function handleCreatePostLyout(){
         setIsExtendentPost(true)
         document.querySelector(".createPost-form").style.height = "170px"
+        document.querySelector(".textPost-input").classList.remove("textPost-hoverEffect");
+    }
+
+    //Controla el label de los iconos de "Agrega"
+    function handleIconLabel(event, element){
+        const label = document.querySelector(`#${element}`)
+        event === "onMouseOver" ? label.style.display = "block" : label.style.display = "none"
     }
 
     return(
@@ -78,7 +91,7 @@ export default function CreatePost(props){
                 <div className="imagePost-container">
                     <img className="profilePicture" src={profilePicture} alt="ProfilePicture" />
                     <textarea
-                        className="textPost-input"
+                        className="textPost-input textPost-hoverEffect"
                         onChange={handleForm}
                         onClick={handleCreatePostLyout}
                         type="text" 
@@ -90,44 +103,71 @@ export default function CreatePost(props){
                 </div>
                 
                 {isExtendentPost && <div className="formPost-options">
-                    <div className="visibility-options" onClick={handleOptionList}>
-                        <p className="checked-name">{checkItem}</p>
-                        <img className="arrowDown-icon" src={arrowDown} alt="arrowDown" />
-                        <img className="arrowUp-icon" src={arrowUp} alt="arrowUp" />
-                        <div className='visibility-list'>
-                            <input
-                                onChange={handleForm}
-                                type="radio"
-                                id="publico"    
-                                name="visibility"
-                                value="publico"
-                                checked={formInfo.visibility === "publico"}
-                            />
-                            {checkItem !== "Publico" &&  <label onClick={()=>setCheckItem("Publico")} htmlFor="publico">Publico</label>} 
-
-                            <input
-                                onChange={handleForm}
-                                type="radio"
-                                id="privado"
-                                name="visibility"
-                                value="privado"
-                                checked={formInfo.visibility === "privado"}
-                            />
-                            {checkItem !== "Privado" &&  <label onClick={()=>setCheckItem("Privado")} htmlFor="privado">Privado</label>}
-
-                            <input
-                                onChange={handleForm}
-                                type="radio"
-                                id="me"
-                                name="visibility"
-                                value="me"
-                                checked={formInfo.visibility === "me"}
-                            />
-                            {checkItem !== "Solo para mi" &&  <label onClick={()=>setCheckItem("Solo para mi")} htmlFor="me">Solo para mi</label>}
-                        </div> 
+                    <div className="postAdd-options">
+                        <span>Agrega</span>
+                        <div>
+                            <img className="post-icon gallery-icon" src={gallery} alt="gallery" 
+                                    onMouseOver={()=>handleIconLabel("onMouseOver","label-photo")} onMouseOut={()=>handleIconLabel("onMouseOut","label-photo")}/>
+                            <label className="icon-label" id="label-photo">Photo</label>
+                        </div>   
+                        <div>
+                            <img className="post-icon play-icon" src={play} alt="play"
+                                    onMouseOver={()=>handleIconLabel("onMouseOver","label-video")} onMouseOut={()=>handleIconLabel("onMouseOut","label-video")}/> 
+                            <label className="icon-label" id="label-video">Video</label>
+                        </div>
+                        <div>
+                            <img className="post-icon microphone-icon" src={microphone} alt="microphone"
+                                    onMouseOver={()=>handleIconLabel("onMouseOver","label-voiceNote")} onMouseOut={()=>handleIconLabel("onMouseOut","label-voiceNote")}/>
+                            <label className="icon-label" id="label-voiceNote">Voice note</label>
+                        </div>
+                        <div>
+                            <img className="post-icon label-icon" src={label} alt="label" 
+                                    onMouseOver={()=>handleIconLabel("onMouseOver","label-tag")} onMouseOut={()=>handleIconLabel("onMouseOut","label-tag")}/>
+                            <label className="icon-label" id="label-tag">Tag</label>
+                        </div>
                     </div>
-                    <button className="submitButton" type="submit">Post</button>
+
+                    <div className="submit-options">
+                        <div className="visibility-options" onClick={handleOptionList}>
+                            <p className="checked-name">{checkItem}</p>
+                            <img className="arrowDown-icon" src={arrowDown} alt="arrowDown" />
+                            <img className="arrowUp-icon" src={arrowUp} alt="arrowUp" />
+                            <div className='visibility-list'>
+                                <input
+                                    onChange={handleForm}
+                                    type="radio"
+                                    id="publico"    
+                                    name="visibility"
+                                    value="publico"
+                                    checked={formInfo.visibility === "publico"}
+                                />
+                                {checkItem !== "Publico" &&  <label onClick={()=>setCheckItem("Publico")} htmlFor="publico">Publico</label>} 
+
+                                <input
+                                    onChange={handleForm}
+                                    type="radio"
+                                    id="privado"
+                                    name="visibility"
+                                    value="privado"
+                                    checked={formInfo.visibility === "privado"}
+                                />
+                                {checkItem !== "Privado" &&  <label onClick={()=>setCheckItem("Privado")} htmlFor="privado">Privado</label>}
+
+                                <input
+                                    onChange={handleForm}
+                                    type="radio"
+                                    id="me"
+                                    name="visibility"
+                                    value="me"
+                                    checked={formInfo.visibility === "me"}
+                                />
+                                {checkItem !== "Solo para mi" &&  <label onClick={()=>setCheckItem("Solo para mi")} htmlFor="me">Solo para mi</label>}
+                            </div> 
+                        </div>
+                        <button className="submitButton" type="submit">Post</button>
+                    </div>
                 </div>}
+
             </form>
         </div>
     );
